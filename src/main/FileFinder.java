285dbde3ee131a141c986e1d5c336be9bf144ea1
DQNEO <dqneoo@gmail.com>
2012-11-02 13:54:56 +0900

    test first and fake it

---
+++ src/main/FileFinder.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileFinder {
+
+	public ArrayList<File> getAllFiles(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		ArrayList<File> ret = new ArrayList<File>();
+		ret.add(new File("."));
+		return ret;
+	}
+
+
+}
