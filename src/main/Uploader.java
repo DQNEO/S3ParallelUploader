c752c95b93bb77a9a2413d515cff4b630821a584
DQNEO <dqneoo@gmail.com>
2012-11-02 16:22:14 +0900

    work successfully when called with one argument as a directory path

--- src/main/Uploader.java
+++ src/main/Uploader.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class Uploader {
 
 	/**
@@ -8,6 +11,11 @@ public class Uploader {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		ArrayList<File> files = FileFinder.find(args[0]);
+
+		for(File f :files) {
+			System.out.println(f);
+		}
 	}
 
 }
