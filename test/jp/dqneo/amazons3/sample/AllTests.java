81c6da6a09d18a81d44f99cece82e12f73d196f4
DQNEO <dqneoo@gmail.com>
2012-11-06 10:04:02 +0900

    added All tests runner

---
+++ test/jp/dqneo/amazons3/sample/AllTests.java
@@ -0,0 +1,16 @@
+package jp.dqneo.amazons3.sample;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    FileFinderTest.class,
+    TimerTest.class,
+    UploadableFileTest.class,
+
+})
+public class AllTests {
+
+}
